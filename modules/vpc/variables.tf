85b2b8cbad35311ab91b00ba954d2a84115a6ecf
Dharshini-V-hcl <dharshini-v@hcltech.com>
2024-12-18 22:51:06 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,11 +1,13 @@
 variable "region" {
   description = "AWS region"
   type        = string
+  default = "us-east-1"
 }
 
 variable "vpc_name" {
   description = "Name of the VPC"
   type        = string
+  default = "terradevops-vpc"
 }
 
 variable "cidr_block" {
@@ -31,12 +33,13 @@ variable "enable_dns_hostnames" {
 variable "public_subnet_cidrs" {
   description = "CIDR blocks for the public subnets"
   type        = list(string)
+  default  = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "CIDR blocks for the private subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]  # Example default values
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]  
 }
 
 variable "availability_zones" {
