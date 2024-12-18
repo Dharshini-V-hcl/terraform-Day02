2d03528b7b331ccd1d515d85b33d21a21665beff
Dharshini-V-hcl <dharshini-v@hcltech.com>
2024-12-18 14:39:26 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,16 +1,19 @@
 variable "region" {
   description = "AWS region"
   type        = string
+  default = "us-east-1"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
+  default = "ami-01816d07b1128cd2d"
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
   type        = string
+  default = "t2.micro"
 }
 
 variable "subnet_id" {
@@ -21,11 +24,13 @@ variable "subnet_id" {
 variable "key_name" {
   description = "Key name for SSH access"
   type        = string
+  default = "my-ssh-key"
 }
 
 variable "instance_name" {
   description = "Name tag for the EC2 instance"
   type        = string
+  default = "Devops-learning"
 }
 
 variable "vpc_id" {
